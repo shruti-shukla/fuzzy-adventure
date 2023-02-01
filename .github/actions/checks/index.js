bcf452b03b7196085f6d1403fa9522a12cae916f
Shruti Shukla <41375094+shruti-shukla@users.noreply.github.com>
2023-02-01 17:33:09 +0000

    test action

--- .github/actions/checks/index.js
+++ .github/actions/checks/index.js
@@ -6,10 +6,11 @@ const repoName = github.context.repo.repo
 const repoOwner = github.context.repo.owner
 const token = core.getInput('token')
 const checkName = core.getInput('checkName')
+// execSync('curl -OL https://github.com/ossf/scorecard/archive/refs/tags/v4.10.2.zip')
 if(token) {
     execSync(`export GITHUB_AUTH_TOKEN=${token}`)
 }
-result = 'Unable to run OSSF checks'
+var result = 'Unable to run OSSF checks'
 if(checkName) {
     result = execSync(`scorecard --repo=github.com/${repoOwner}/${repoName} --checks=${checkName}`).toString();
 } else {
