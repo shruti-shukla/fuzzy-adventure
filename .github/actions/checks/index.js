04da69d00ebcbcbbf46bf9b4c94be7042d1b1b52
Shruti Shukla <41375094+shruti-shukla@users.noreply.github.com>
2023-02-01 16:50:14 +0000

    test changes

--- .github/actions/checks/index.js
+++ .github/actions/checks/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {execSync} from 'child_process'
+
+const repoName = github.context.repo.repo
+const repoOwner = github.context.repo.owner
+const token = core.getInput('token')
+const checkName = core.getInput('checkName')
+if(token) {
+    execSync(`export GITHUB_AUTH_TOKEN=${token}`)
+}
+result = 'Unable to run OSSF checks'
+if(checkName) {
+    result = execSync(`scorecard --repo=github.com/${repoOwner}/${repoName} --checks=${checkName}`).toString();
+} else {
+    result = execSync(`scorecard --repo=github.com/${repoOwner}/${repoName}`).toString();
+}
+console.log(result);
